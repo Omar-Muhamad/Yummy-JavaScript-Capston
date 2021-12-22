ea8760b998b45e3033feac8306a050ca53330ebb
keroloussamy <keroloussamy98@gmail.com>
2021-12-22 21:44:06 +0200

    Add like feature

--- src/mealAPI.js
+++ src/mealAPI.js
@@ -1,8 +1,11 @@
+import involementApi from './involvementAPI.js';
+
 async function fetchMeals() {
   const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
   const response = await fetch(URL);
   return response.json();
 }
+
 const displayMeals = () => {
   fetchMeals()
     .then((data) => {
@@ -13,17 +16,22 @@ const displayMeals = () => {
     <div class="col-md-4 col-sm-6 text-center pb-5" data-id=${item.idMeal}>
       <img class="img-thumbnail rounded" src=${item.strMealThumb} alt=${item.strMeal}>
       <h5 class="py-2">${item.strMeal}</h5>
-      <p class="likes text-end me-2"></p>
+      <div class="d-flex gap-5 justify-content-center">
+        <p class="likes text-end"></p>
+        <i class="fas fa-heart fs-4 text-danger"></i>
+      </div>
       <button class="btn btn-primary m-2">Comments</button>
       <button class="btn btn-primary">Reservations</button>
     </div>
     `;
       }
       document.querySelector('main').innerHTML = html;
+      const hearts = document.querySelectorAll('.fa-heart');
+      [...hearts].forEach((heart) => { heart.addEventListener('click', involementApi.updateLikes); });
     })
     .catch((error) => {
       document.querySelector('main').innerHTML = error;
     });
 };
 
-module.exports = { displayMeals };
+export default { displayMeals };
