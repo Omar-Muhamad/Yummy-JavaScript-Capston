0d59584f450e6667d36103f30c1ee6910891d614
keroloussamy <keroloussamy98@gmail.com>
2021-12-23 22:12:07 +0200

    Add Categories and item counter

--- src/meals.js
+++ src/meals.js
@@ -1,13 +1,17 @@
-import { fetchLikes, fetchMeals, postLikes } from './API.js';
+import {
+  fetchLikes, fetchMeals, postLikes, fetchCategories,
+} from './API.js';
 import renderCommentsPopup from './commentsPopup.js';
 
 const displayLikes = () => {
   fetchLikes()
     .then((data) => {
-      for (let i = 0; i < 6; i += 1) {
+      for (let i = 0; i < data.length; i += 1) {
         const parent = document.querySelector(`[data-id="${data[i].item_id}"]`);
-        const paragraph = parent.querySelector('.likes');
-        paragraph.textContent = `Likes ${data[i].likes}`;
+        if (parent) {
+          const paragraph = parent.querySelector('.likes');
+          paragraph.textContent = `Likes ${data[i].likes}`;
+        }
       }
     });
 };
@@ -20,28 +24,28 @@ const updateLikes = (event) => {
     });
 };
 
-const displayMeals = () => {
-  fetchMeals()
+const displayMeals = (categoryName) => {
+  fetchMeals(categoryName)
     .then((data) => {
       let html = '';
-      for (let i = 0; i < 6; i += 1) {
+      for (let i = 0; i < data.meals.length; i += 1) {
         const item = data.meals[i];
         html += `
     <div class="col-md-4 col-sm-6 text-center pb-5" data-id=${item.idMeal}>
       <img class="img-thumbnail rounded" src=${item.strMealThumb} alt=${item.strMeal}>
       <h5 class="py-2">${item.strMeal}</h5>
       <div class="d-flex gap-5 justify-content-center">
-        <p class="likes text-end"></p>
+        <p class="likes text-end">Likes 0</p>
         <i class="fas fa-heart fs-4 text-danger"></i>
       </div>
-      <p class="likes text-end me-2"></p>
       <button class="btn btn-primary m-2 commentBtn">Comments</button>
       <button class="btn btn-primary">Reservations</button>
     </div>
     `;
       }
-      const main = document.querySelector('main');
+      const main = document.querySelector('.main');
       main.innerHTML = html;
+
       main.addEventListener('click', (event) => {
         if (event.target.classList.contains('commentBtn')) {
           const parent = event.target.parentNode;
@@ -49,13 +53,39 @@ const displayMeals = () => {
           renderCommentsPopup(mealID);
         }
       });
+
       const hearts = document.querySelectorAll('.fa-heart');
       [...hearts].forEach((heart) => { heart.addEventListener('click', updateLikes); });
       displayLikes();
     })
     .catch((error) => {
-      document.querySelector('main').innerHTML = error;
+      document.querySelector('.main').innerHTML = error;
     });
 };
 
-export default displayMeals;
+const displayCategories = () => {
+  const select = document.querySelector('.form-select');
+  fetchCategories()
+    .then((data) => {
+      data.categories.forEach((item) => {
+        const opt = document.createElement('option');
+        opt.value = item.strCategory;
+        opt.innerHTML = item.strCategory;
+        select.appendChild(opt);
+      });
+    });
+};
+
+const dataLength = (categoryName) => fetchMeals(categoryName)
+  .then((data) => data.meals.length);
+
+const displayDataLength = (categoryName) => {
+  dataLength(categoryName).then((data) => {
+    const navItem = document.querySelector('.disabled');
+    navItem.textContent = `${data} ${categoryName} recipes`;
+  });
+};
+
+export {
+  displayMeals, displayDataLength, displayLikes, displayCategories,
+};
